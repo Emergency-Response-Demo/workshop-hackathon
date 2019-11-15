78d33a14683ca9325d594213bffba7a65a9cad72
Shaaf <sshaaf@redhat.com>
2019-11-15 12:07:09 +0100

    add fixes for vertx incident-services

--- solutions/vertx/incident-service/src/main/java/com/example/incident/model/Incident.java
+++ solutions/vertx/incident-service/src/main/java/com/example/incident/model/Incident.java
@@ -1,5 +1,7 @@
 package com.example.incident.model;
 
+import java.util.UUID;
+
 public class Incident {
 
   private String id;
@@ -20,98 +22,85 @@ public class Incident {
 
   private String status;
 
+
+  public Incident() {
+    id = UUID.randomUUID().toString();
+    status = IncidentStatus.REPORTED.toString();
+    timestamp = System.currentTimeMillis();
+  }
+
   public String getId() {
     return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
   }
 
   public String getLat() {
     return lat;
   }
 
+  public void setLat(String lat) {
+    this.lat = lat;
+  }
+
   public String getLon() {
     return lon;
+  }
+
+  public void setLon(String lon) {
+    this.lon = lon;
   }
 
   public Integer getNumberOfPeople() {
     return numberOfPeople;
   }
 
+  public void setNumberOfPeople(Integer numberOfPeople) {
+    this.numberOfPeople = numberOfPeople;
+  }
+
   public Boolean isMedicalNeeded() {
     return medicalNeeded;
+  }
+
+  public void isMedicalNeeded(Boolean medicalNeeded) {
+    this.medicalNeeded = medicalNeeded;
   }
 
   public String getVictimName() {
     return victimName;
   }
 
+  public void setVictimName(String victimName) {
+    this.victimName = victimName;
+  }
+
   public String getVictimPhoneNumber() {
     return victimPhoneNumber;
+  }
+
+  public void setVictimPhoneNumber(String victimPhoneNumber) {
+    this.victimPhoneNumber = victimPhoneNumber;
   }
 
   public Long getTimestamp() {
     return timestamp;
   }
 
+  public void setTimestamp(Long timestamp) {
+    this.timestamp = timestamp;
+  }
+
   public String getStatus() {
     return status;
   }
 
-  public static class Builder {
-
-    private final Incident incident;
-
-    public Builder() {
-      incident = new Incident();
-    }
-
-    public Builder(String id) {
-      incident = new Incident();
-      incident.id = id;
-    }
-
-    public Builder lat(String lat) {
-      incident.lat = lat;
-      return this;
-    }
-
-    public Builder lon(String lon) {
-      incident.lon = lon;
-      return this;
-    }
-
-    public Builder numberOfPeople(Integer numberOfPeople) {
-      incident.numberOfPeople = numberOfPeople;
-      return this;
-    }
-
-    public Builder medicalNeeded(Boolean medicalNeeded) {
-      incident.medicalNeeded = medicalNeeded;
-      return this;
-    }
-
-    public Builder victimName(String victimName) {
-      incident.victimName = victimName;
-      return this;
-    }
-
-    public Builder victimPhoneNumber(String victimPhoneNumber) {
-      incident.victimPhoneNumber = victimPhoneNumber;
-      return this;
-    }
-
-    public Builder timestamp(Long timestamp) {
-      incident.timestamp = timestamp;
-      return this;
-    }
-
-    public Builder status(String status) {
-      incident.status = status;
-      return this;
-    }
-
-    public Incident build() {
-      return incident;
-    }
-
+  public void setStatus(String status) {
+    this.status = status;
   }
+
+
+
 }
